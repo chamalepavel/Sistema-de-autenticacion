62a9615d3501440d1c2ab817d3761f236a36dbd7
PavelintheMatrix <166340133+chamalepavel@users.noreply.github.com>
2025-05-25 20:30:37 -0700

    auth.middleware.js

---
+++ models/controllers/middlewares/auth.middleware.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Token no proporcionado' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token inválido' });
+  }
+};
